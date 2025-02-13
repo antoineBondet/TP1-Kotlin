32ba0c0ab97bf38b4c2290e394558ce2d8d4f88e
antoineBondet <91895104+antoineBondet@users.noreply.github.com>
2025-02-13 15:00:18 +0100

    Réalisation de la question 4

--- ex1/src/main/kotlin/ex1.kt
+++ ex1/src/main/kotlin/ex1.kt
@@ -1,6 +1,7 @@
 import kotlin.math.sqrt
 
 fun main() {
+    // Générer une liste de 1000 nombres aléatoires compris entre 20 et 40. Calculer la moyenne, la variance et l’écart type de cette liste.
     val ListeNbr = "liste de  1000 nombres compris entre 20 et 40:"
     val rndList = List(1000){(20..40).random()}
     println("$ListeNbr $rndList")
@@ -20,4 +21,17 @@ fun main() {
     val ecartType = sqrt(variance)
     val EcartType = "Ecart Type:"
     println ("$EcartType $ecartType")
+
+val histogramme = mutableMapOf<Int, Int>()
+for (i in rndList) {
+    histogramme[i] = histogramme.getOrDefault(i, 0) + 1
+}
+val modalite = histogramme.maxByOrNull { it.value }?.key
+val frequence = histogramme[modalite]
+val Modalite = "Modalite:"
+println ("$Modalite $modalite")
+val Frequence = "Frequence:"
+println ("$Frequence $frequence")
+
+
 }
